01e4286f45f9997a4e29a8f849c4da8ca6d89870
YiMing <ericdreamwithvic@gmail.com>
2021-11-18 14:56:59 -0500

    adding table to c2Page

--- goat_of_f1-web/src/pages/c2page.js
+++ goat_of_f1-web/src/pages/c2page.js
@@ -8,6 +8,26 @@ import settings from "../settings";
 import axios from "axios";
 import { randDarkColor } from '../utils/utils'
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 function C2Page() {
     const [scoreIncrease, setAverageAgewisePoints] = useState([{ countructorId: 0, year: '', totalPoint: 0}])
     const [constructorList, setConstructorList] = useState([])
@@ -39,6 +59,32 @@ function C2Page() {
         setConstructorList(response.data.result.data.constructors)
     }
 
+    // Get the investable constructor list
+    function generateConstructorList() {
+        const listItem = constructorList.map((element, index) => {
+            return(
+                <div style={{
+                    borderRight: 'solid 3px ' + settings.Colors.mainColor,
+                    borderBottom: 'solid 3px ' + settings.Colors.mainColor,
+                }}>
+                    <ListItem>
+                        <ListItemText
+                            key={index}
+                            primary={element.name.toUpperCase()}
+                        />
+                    </ListItem>
+                </div>
+            )
+        })
+
+        return (
+            <List class="hide-scrollbar" style={{maxHeight: '100%', overflow: 'auto'}}>
+                {listItem}
+            </List>
+        )
+    }
+    
+    // Function A
     function constructorLineChart() {
         const options = {
             scales: {
@@ -68,7 +114,7 @@ function C2Page() {
             })
         };
         return(
-            <div style={{width: '70%', height: 500}}>
+            <div className="c2-function-components">
                 <div className='header'>
                     <h1 className='title page-title'> Total Points (2015-2017)</h1>
                     <div className='links'>
@@ -78,33 +124,47 @@ function C2Page() {
                     >
                     </a>
                     </div>
-                </div>s
+                </div>
                 <Line data={data} options={options} />
             </div>
         )
     }
 
-    function generateConstructorList() {
-        const listItem = constructorList.map((element, index) => {
-            return(
-                <div style={{
-                    borderRight: 'solid 3px ' + settings.Colors.mainColor,
-                    borderBottom: 'solid 3px ' + settings.Colors.mainColor,
-                }}>
-                    <ListItem>
-                        <ListItemText
-                            key={index}
-                            primary={element.name.toUpperCase()}
-                        />
-                    </ListItem>
-                </div>
-            )
-        })
-
+    // Function B
+    function constructorStatTable() {
         return (
-            <List class="hide-scrollbar" style={{maxHeight: '100%', overflow: 'auto'}}>
-                {listItem}
-            </List>
+            <div className="c2-function-components">
+            <h1 className='title page-title'> Total Points (2015-2017)</h1>
+            <TableContainer style={{marginTop: 50, marginBottom: 50}} component={Paper}>
+                <Table size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                    <TableRow
+                        key={row.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <TableCell component="th" scope="row">
+                        {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.calories}</TableCell>
+                        <TableCell align="right">{row.fat}</TableCell>
+                        <TableCell align="right">{row.carbs}</TableCell>
+                        <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+                </Table>
+            </TableContainer>
+            </div>
         )
     }
 
@@ -130,6 +190,8 @@ function C2Page() {
                 flexDirection: 'column'
             }}>
                 {constructorLineChart()}
+                <div style={{height: 100}}/>
+                {constructorStatTable()}
             </div>
         </div>
     )
