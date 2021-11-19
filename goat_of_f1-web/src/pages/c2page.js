c97b1ff9514bf7e8db9aad51175890f356e703c5
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2021-11-19 10:48:58 -0500

    add hover and explainBoard

--- goat_of_f1-web/src/pages/c2page.js
+++ goat_of_f1-web/src/pages/c2page.js
@@ -16,58 +16,43 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import explainBoard from '../component/explainBoard'
 
 function C2Page() {
-    const [scoreIncrease, setAverageAgewisePoints] = useState([{ countructorId: 0, year: '', totalPoint: 0}])
     const [constructorList, setConstructorList] = useState([])
+    const [selectedTeam, setSelectedTeam] = useState({})
     const startYear = 2015
     const endYear = 2017
-
-    useEffect(() => {
-        getScoreIncrease();
-        getContructorList();
-    }, [])
     
-    const getScoreIncrease = async () => {
-        // const response = await axios.get(averageAgewisePointsUrl)
-        const fakeResponse = [
-            { countructorId: 0, year: 2015, totalPoint: 100 },
-            { countructorId: 0, year: 2016, totalPoint: 132 },
-            { countructorId: 0, year: 2017, totalPoint: 166 },
-          ];
-        // setAgewisePoints(response.data)
-        setAverageAgewisePoints(fakeResponse)
-    }
+    useEffect(() => {
+        getConstructorList();
+    }, [])
 
-    const getContructorList = async () => {
+    const getConstructorList = async () => {
         const constructorUrl = settings.apiHostURL + '/c2/investable-constructors'
         let response = await axios.get(constructorUrl)
-        // let filterList = response.data.MRData.DriverTable.Constructors.map((element) => {
-        //     filterList.{'id': element.constructorId, 'name': element.name}
-        // })
-        setConstructorList(response.data.result.data.constructors)
+        if(response.data.result.data.constructors) {
+            setConstructorList(response.data.result.data.constructors)
+        }
+        let defaultTeam = response.data.result.data.constructors ? response.data.result.data.constructors[0] : {'name': 'None'}
+        setSelectedTeam(defaultTeam)
+    }
+
+    function constructTimeRange() {
+        return `${startYear}-${endYear}`
     }
 
     // Get the investable constructor list
     function generateConstructorList() {
         const listItem = constructorList.map((element, index) => {
             return(
-                <div style={{
-                    borderRight: 'solid 3px ' + settings.Colors.mainColor,
-                    borderBottom: 'solid 3px ' + settings.Colors.mainColor,
-                }}>
-                    <ListItem>
+                <div 
+                    style={{
+                        borderBottom: 'solid 2.5px ' + settings.Colors.mainColor,
+                        fontFamily: settings.Font.major
+                    }}
+                >
+                    <ListItem className="list-item-component">
                         <ListItemText
                             key={index}
                             primary={element.name.toUpperCase()}
@@ -78,9 +63,19 @@ function C2Page() {
         })
 
         return (
-            <List class="hide-scrollbar" style={{maxHeight: '100%', overflow: 'auto'}}>
-                {listItem}
-            </List>
+            <div style={{
+                position: 'fixed',
+                left: 75,
+                top: 100,
+                height: 400,
+                width: 300,
+                border: 'solid 10px ' + settings.Colors.mainColor,
+                borderTopRightRadius: 25
+            }}>
+                <List class="hide-scrollbar" style={{maxHeight: '100%', overflow: 'auto'}}>
+                    {listItem}
+                </List>
+            </div>
         )
     }
     
@@ -98,7 +93,7 @@ function C2Page() {
         for(var i = startYear; i <= endYear; i++) {
             yearLabel.push(i)
         }
-
+        
         const data = {
             labels: yearLabel,
             datasets: constructorList.map((element, index) => {
@@ -113,10 +108,11 @@ function C2Page() {
                 })
             })
         };
+
         return(
             <div className="c2-function-components">
                 <div className='header'>
-                    <h1 className='title page-title'> Total Points (2015-2017)</h1>
+                    <h1 className='title page-title'> Total Points ({constructTimeRange()})</h1>
                     <div className='links'>
                     <a
                         className='btn btn-gh'
@@ -130,36 +126,51 @@ function C2Page() {
         )
     }
 
+    function createRowData(year, budgets, avgPitTime, errors) {
+        return { year, budgets, avgPitTime, errors}
+    }
+
+    function constructTableRows() {
+        if(Object.keys(selectedTeam).length === 0) return []
+        let tableRows = []
+        for(let i = startYear; i <= endYear; i++) {
+            let currIdx = i-startYear
+            if(selectedTeam.budgets && selectedTeam.avg_pit_time && selectedTeam.errors) {
+                let tuple = createRowData(i, selectedTeam.budgets[currIdx], selectedTeam.avg_pit_time[currIdx], selectedTeam.errors[currIdx])
+                tableRows.push(tuple)
+            }
+        }
+        return tableRows
+    }
+
     // Function B
     function constructorStatTable() {
         return (
             <div className="c2-function-components">
-            <h1 className='title page-title'> Total Points (2015-2017)</h1>
+            <h1 className='title page-title'> {selectedTeam.name} Stats {constructTimeRange()}</h1>
             <TableContainer style={{marginTop: 50, marginBottom: 50}} component={Paper}>
-                <Table size="small" aria-label="a dense table">
+                <Table size="medium" aria-label="a dense table">
                 <TableHead>
                     <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Year</TableCell>
+                        <TableCell align="left">Budget(M)</TableCell>
+                        <TableCell align="left">Average Pit Time(s)</TableCell>
+                        <TableCell align="left">Errors</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                    <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">
-                        {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
+                    {constructTableRows().map((row) => (
+                        <TableRow
+                            key={row.year}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                            {row.year}
+                            </TableCell>
+                            <TableCell align="left">{row.budgets}</TableCell>
+                            <TableCell align="left">{row.avgPitTime}</TableCell>
+                            <TableCell align="left">{row.errors}</TableCell>
+                        </TableRow>
                     ))}
                 </TableBody>
                 </Table>
@@ -171,18 +182,7 @@ function C2Page() {
     return (
         <div>
             <Header/>
-            <div style={{
-                position: 'fixed',
-                left: 75,
-                top: 100,
-                height: 500,
-                width: 400,
-                borderTop: 'solid 10px ' + settings.Colors.mainColor,
-                borderRight: 'solid 10px ' + settings.Colors.mainColor,
-                borderTopRightRadius: 25
-            }}>
-                {generateConstructorList()}
-            </div>
+            {generateConstructorList()}
             <div style={{
                 marginTop: 100,
                 marginLeft: 550,
@@ -190,8 +190,30 @@ function C2Page() {
                 flexDirection: 'column'
             }}>
                 {constructorLineChart()}
+                <div style={{marginTop: 50}} className="c2-function-components">
+                    {explainBoard(
+                        "Total Points Explanation", 
+                        [
+                            "1. For each year the increase rate should be over 20%",
+                            "2. Only listing the top K team with the MAX point differences (End - Start)",
+                        ]
+                    )}
+                </div>
                 <div style={{height: 100}}/>
                 {constructorStatTable()}
+                <div style={{marginTop: 50}} className="c2-function-components">
+                    {explainBoard(
+                        "Team Stats Explanation", 
+                        [
+                            "Teams are improving due to different perspectives",
+                            "1. Budget - The success is simply increasing the funding",
+                            "---",
+                            "2. Pit Stop Time - Changing race car components is also another battle, great team work show at pit stop time",
+                            "---",
+                            "3. Errors - Errors often is unacceptable, team will cost hundred thousands dollars due to errors" 
+                        ]
+                    )}
+                </div>
             </div>
         </div>
     )
